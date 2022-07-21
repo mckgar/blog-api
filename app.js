59824b154cb63fca71bec9439953889d31d82a39
mckgar <kmgarner23@outlook.com>
2022-07-21 14:50:43 -0600

    Connected DB

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB setup
+const mongoDb = process.env.DB_URI;
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`)
