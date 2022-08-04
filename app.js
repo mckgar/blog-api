0308e45e87c8e62bf9b63049f782b96796b5b37b
mckgar <kmgarner23@outlook.com>
2022-08-04 14:05:26 -0600

    Added error handler

--- app.js
+++ app.js
@@ -18,6 +18,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/user', routes.user);
 app.use('/login', routes.login);
 
+app.use((req, res, next) => {
+  return res.status(404).json(
+    {
+      message: '404: Page not found'
+    }
+  )
+})
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  return res.status(err.status || 500).json(
+    {
+      message: "Oops, an error has occured"
+    }
+  )
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`)
 });
